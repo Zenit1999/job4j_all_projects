e65cea17c4e5497d063282f227ef88b7b53c8541
Konstantinos <konstantantinoskonstanta2023@mail.ru>
2022-12-03 19:18:46 +0300

    Test commit6

--- src/main/java/newtask/Firstclass.java
+++ src/main/java/newtask/Firstclass.java
@@ -2,6 +2,10 @@ package newtask;
 
 public class Firstclass {
     public static void main(String[] args) {
+        int a = 25;
+        long b = (long) 25.52;
         System.out.println("First test commit a new project");
+        System.out.println(a);
+        System.out.println(b);
     }
 }
